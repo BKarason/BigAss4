68f317f7d3b74535c58689105358106fcfe3da3c
Birkir K <birkir17@ru.is>
2019-03-21 18:35:28 +0000

    reyna ad adda user a server

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addUser } from '../../actions/userActions';
+import { socket } from '../../services/socketService';
 
 class LoginForm extends React.Component{
     constructor(props) {
@@ -15,10 +16,18 @@ class LoginForm extends React.Component{
         });
     }
     onSubmit(e) {
-        e.preventDefault();
+        //e.preventDefault();
         const { addUser } = this.props;
         const { name } = this.state;
         addUser(name);
+        emitUserToServer(name);
+    }
+    emitUserToServer(e) {
+        socket.emit("adduser", e, function(available){
+            if (available){
+                // The "GnoMe" username is not taken!
+            }
+        });
     }
     render() {
         const { name } = this.state;
