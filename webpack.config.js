4d0fe9510caf602f58f7e6e4ec77e45f5ed3f346
BKarason <34065552+BKarason@users.noreply.github.com>
2019-03-21 14:51:59 +0000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    mode: "development",
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: "style-loader"
+                },
+                {
+                    loader: "css-loader"
+                }]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.css']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ title: 'Chat.io', template: './index.html', inject: 'body' })
+    ],
+    devServer: {
+        open: true,
+        compress: true,
+        historyApiFallback: true,
+        port: 3000
+    }
+};
