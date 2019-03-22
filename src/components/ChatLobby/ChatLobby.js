d7f8831ac1e84fe94955d23169a5ac63f715b015
Birkir K <birkir17@ru.is>
2019-03-22 17:40:33 +0000

    create room done

--- src/components/ChatLobby/ChatLobby.js
+++ src/components/ChatLobby/ChatLobby.js
@@ -12,13 +12,30 @@ class ChatLobby extends React.Component {
         super(props);
         this.state = {
             availRooms: {},
+            roomName: ''
+        }
+    }
+    createRoom(e) {
+        e.preventDefault();
+        const roomName = this.state.roomName;
+        console.log(roomName);
+        if(roomName !== ''){
+            socket.emit('joinroom', { room: roomName }, success => {
+                if(success){
+                    socket.emit('rooms');
+                    console.log("room  created!")
+                }
+                else{
+                    console.log('Error creating room!');
+                }
+            });
         }
     }
     
     joinRoom(e) {
         e.preventDefault();
         console.log("hurr i em");
-        socket.emit('joinroom', { room: e.target.name}, success => {
+        socket.emit('joinroom', { room: e.target.name }, success => {
             if(success){
                 socket.emit('rooms');
                 console.log('joined room:');
@@ -28,16 +45,30 @@ class ChatLobby extends React.Component {
             }
         });
     }
+    onInput(e) {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
     render(){
         var allRooms = [];
+        const { roomName } = this.state.roomName;
+        //const { roomName } = this.state.roomName;
         Object.keys(this.state.availRooms).forEach(key => {
             allRooms.push(<div key={key}><button type="button" key={key} name={key} onClick={e => this.joinRoom(e)}>{ key }</button></div>);
         });
-        
         return (
             <div>
                 <h1 className="text-center" style={{marginTop: 40 }}>Chat.io</h1>
                 { allRooms }
+                <form action="" name={ roomName } onSubmit={e => this.createRoom(e)} className="form-horizontal">
+                    <div className="form-goup">
+                    <input type="text" name="roomName" id="roomName" value={ roomName } onChange={e => this.onInput(e)} />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create room!" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
