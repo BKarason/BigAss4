07a0e1b73626de2a4c9932e591419372fdebb66b
Birkir K <birkir17@ru.is>
2019-03-22 15:40:31 +0000

    kill me now fk u react

--- src/components/ChatLobby/ChatLobby.js
+++ src/components/ChatLobby/ChatLobby.js
@@ -3,20 +3,35 @@ import { socket } from '../../services/socketService';
 
 
 class ChatLobby extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            availRooms: {}
-        }
-    }
     componentDidMount() {
         socket.emit('rooms');
         socket.on('roomlist', availRooms => this.setState({availRooms}));
     }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            availRooms: {},
+        }
+    }
+    
+    joinRoom(e) {
+        e.preventDefault();
+        console.log("hurr i em");
+        socket.emit('joinroom', { room: e.target.name}, success => {
+            if(success){
+                socket.emit('rooms');
+                console.log('joined room:');
+            }
+            else{
+                console.log('join failed');
+            }
+        });
+    }
     render(){
         var allRooms = [];
-        Object.keys(this.state.availRooms).forEach(function(key) {
-            allRooms.push(<div key={key}> { key } </div>)
+        Object.keys(this.state.availRooms).forEach(key => {
+            allRooms.push(<div key={key}><button type="button" key={key} name={key} onClick={e => this.joinRoom(e)}>{ key }</button></div>);
         });
         
         return (
