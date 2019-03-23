5cae664cd82f2bc3f2b8517b4c573fa866714d39
Birkir K <birkir17@ru.is>
2019-03-23 23:17:53 +0000

    private msg done, en bara eitt message i einu

--- src/components/PrivateChat/PrivateChat.js
+++ src/components/PrivateChat/PrivateChat.js
@@ -5,13 +5,13 @@ import { socket } from '../../services/socketService';
 class PrivateChat extends React.Component{
     componentDidMount() {
         socket.on('recv_privatemsg', (username, message) => {
-            this.setState({messages: messages.push(message), sender: username });
+            this.setState({messages: message, sender: username });
         });
     }
     constructor(props) {
         super(props);
         this.state = {
-            messages: [],
+            messages: '',
             sendmessage: "",
             recipent: "",
             sender: ""
@@ -23,15 +23,26 @@ class PrivateChat extends React.Component{
         });
     }
     sendPrivateMessage(e){
-
+        e.preventDefault();
+        if(this.state.recipent !== '' && this.state.sendmessage !== ''){
+            socket.emit('privatemsg', { nick: this.state.recipent, message: this.state.sendmessage}, success => {
+                if(success){
+                    console.log('Message sent');
+                    this.setState({ sendmessage: '', recipent: ''});
+                }
+            });
+        }
     }
     render() {
+        //const { messages } = this.state.messages;
         return (
             <>
                 <div className="chat-window" id="prvtmsg">
                     <h3 className="text-center header">Private messages:</h3>
                     <div className="input-container">
-                        <input type="text" placeholder="Type your message..." value={ sendmessage } name="sendmessage" onChange={e => this.onInput(e)}/>
+                        <p> {this.state.sender } - { this.state.messages }</p>
+                        <input type="text" placeholder="Type recipent..." value={ this.state.recipent } name="recipent" onChange={e => this.onInput(e)}/>
+                        <input type="text" placeholder="Type your message..." value={ this.state.sendmessage } name="sendmessage" onChange={e => this.onInput(e)}/>
                         <button type="button" onClick={e => this.sendPrivateMessage(e)}>Send</button>
                     </div>
                 </div>
